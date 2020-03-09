60053d80677dc6b164c36aaea74187716e579506
zhangml123 <s15532292192@163.com>
2020-03-09 12:17:56 +0800

    update files

--- plugin.rb
+++ plugin.rb
@@ -6,15 +6,5 @@
 # author: Joffrey Jaffeux
 # url: https://github.com/discourse/discourse-calendar
 
-gem "holidays", "8.0.0", require: false
-
-[
-  "../lib/calendar_settings_validator.rb",
-].each { |path| load File.expand_path(path, __FILE__) }
-
-enabled_site_setting :calendar_enabled
 
 
-
-PLUGIN_NAME ||= "calendar"
-DATA_PREFIX ||= "data-calendar-"

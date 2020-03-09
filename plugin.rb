b597fbabd8509bee10c43d4c7730df481b366594
zhangml123 <s15532292192@163.com>
2020-03-09 11:35:01 +0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -5,3 +5,19 @@
 # version: 0.2
 # author: Joffrey Jaffeux
 # url: https://github.com/discourse/discourse-calendar
+
+gem "holidays", "8.0.0", require: false
+
+[
+  "../lib/calendar_settings_validator.rb",
+].each { |path| load File.expand_path(path, __FILE__) }
+
+enabled_site_setting :calendar_enabled
+
+register_asset "stylesheets/vendor/fullcalendar.min.css"
+register_asset "stylesheets/common/discourse-calendar.scss"
+register_asset "stylesheets/mobile/discourse-calendar.scss", :mobile
+register_asset "stylesheets/desktop/discourse-calendar.scss", :desktop
+
+PLUGIN_NAME ||= "calendar"
+DATA_PREFIX ||= "data-calendar-"
